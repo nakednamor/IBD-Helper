91abcbf900ff00c62292b8815deabcf72cb90645
Roman Zillinger <r.zillinger@smartassistant.com>
2019-01-13 16:35:08 +0100

    changed layout of main activity add new activity

---
+++ app/src/main/java/net/zillinger/roman/ibdhelper/BowelAdd.java
@@ -0,0 +1,13 @@
+package net.zillinger.roman.ibdhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BowelAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bowel_add);
+    }
+}
